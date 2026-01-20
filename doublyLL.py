38448e298564febdad6d5b1f3eb4123aba20f01a
Prolay <prolaydas130@Gmail.com>
2026-01-21 01:00:21 +0530

    +Stack

--- doublyLL.py
+++ doublyLL.py
@@ -29,6 +29,41 @@ class doublyLL:
         self.head.prev = temp
         self.head = temp
 
+    def insertAtMid(self, value, x):
+        t = self.head
+        while t.next != None:
+            if t.data == x:
+                break
+            else:
+                t = t.next
+        temp = Node(value)
+        temp.next = t.next
+        t.next.prev = temp
+        t.next = temp
+        temp.prev = t
+        # print(temp)
+        # print(temp.data)
+        # print(temp.next)
+        # print(temp.prev)
+
+    def deleteNode(self, value):
+        if self.head == None:
+            print("Linked List is empty.")
+            return
+        t = self.head
+        if t.data == value:
+            self.head = t.next
+            self.head.prev = None
+            return
+        while t.next != None:
+            if t.data == value:
+                t.prev.next = t.next
+                t.next.prev = t.prev
+                return
+            t = t.next
+            if t.data == value:
+                t.prev.next = None
+
     def printDoublyLL(self):
         t1 = self.head
         while t1.next != None:
@@ -42,4 +77,6 @@ obj.insertAtEnd(20)
 obj.insertAtEnd(30)
 obj.insertAtEnd(40)
 obj.insertAtBeg(10)
+obj.insertAtMid(35, 30)
+obj.deleteNode(35)
 obj.printDoublyLL()
