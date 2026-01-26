3bf1df6cb27fbf70c77ecb4ae770a090990892ad
Prolay <prolaydas130@Gmail.com>
2026-01-26 22:10:33 +0530

    best time to buy and sell stock

---
+++ LEETCODE-BLIND 75/leetcode75.py
@@ -0,0 +1,22 @@
+# QESTION NO: 121
+from typing import List
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        l, r = 0, 1
+        maxP = 0
+
+        while r < len(prices):
+            if prices[l] < prices[r]:
+                profit = prices[r] - prices[l]
+                maxP = max(maxP, profit)
+            else:
+                l = r
+            r += 1
+        return maxP
+
+
+obj = Solution()
+prices = [7, 1, 5, 3, 6, 4]
+result = obj.maxProfit(prices)
+
+print("Max Profit:", result)
