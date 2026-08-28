0a6478751233e89dada23c111a6809e4f888368b
Nandita Shit <nanditashit1999@gmail.com>
2026-08-29 01:06:05 +0530

    some changes in buy sell

--- practice/stockBuySell.py
+++ practice/stockBuySell.py
@@ -1,20 +1,15 @@
-prices=[7,1,5,3,9,4]
-def maxProfit(prices):    
-    max_profit=  0
-    buy_index= 0
-    best_buy_index =0
-    best_sell_index = 0
-    for i in range(len(prices)):
-        if prices[i] < prices[buy_index]:
-            buy_index = i
+prices=[1,2,4]
+def maxProfit(prices):
+    max_profit= 0
+    min_price=prices[0]
+    for j in range(1 , len(prices)):
+        if prices[j]< min_price:
+            min_price= prices[j]
         else:
-            profit = prices[i]- prices[buy_index]
+            profit= (prices[j]- min_price)
             if profit > max_profit:
                 max_profit= profit
-                best_buy_index = buy_index
-                best_sell_index= i
-                
-    return (best_buy_index + best_sell_index)
-        
+    return max_profit
+    
 
 print(maxProfit(prices))
